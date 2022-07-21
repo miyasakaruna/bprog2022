24a1191debdbbe40f562741dd043f578843b4c5b
miyasakaruna <runamysk@gmail.com>
2022-07-21 16:13:56 +0900

    ch5

--- src/Caesar.hs
+++ src/Caesar.hs
@@ -18,7 +18,7 @@ encode n xs = [ shift n x | x <- xs ]
 
 table :: [Float]
 table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0
-        , 0.2, 0.8, 0.4, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0
+        , 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0
         , 6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1
         ]
 
@@ -27,3 +27,33 @@ percent n m = (fromIntegral n / fromIntegral m) * 100
 
 count :: Char -> (String -> Int)
 count x xs = length [ x' | x' <- xs, x == x']
+
+freqs :: String -> [Float]
+freqs xs = [ percent (count x xs) n | x <- ['a' .. 'z']]
+    where 
+        n = length xs
+
+sample :: String
+sample = "abbcccddddeeeee"
+
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [ (o - e)^2 / e | (o, e) <- zip os es ]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
+
+table' :: [Float]
+table' = freqs sampleAngou
+
+sampleAngou :: String
+sampleAngou = "kdvnhoo lv ixq"
+
+crack :: String -> String
+crack xs = encode (-factor) xs
+    where
+        factor = head (positions (minimum chitab) chitab)
+        chitab = [chisqr (rotate n table') table | n <- [0 .. 25]]
+        table' = freqs xs
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [ i | (x', i) <- zip xs [0 ..], x == x' ]
